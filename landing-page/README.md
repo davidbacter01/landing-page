de11179af35c898c8774c609cd5527324628e5b5
davidbacter01 <davidbacter01@gmail.com>
2020-06-05 01:23:06 +0300

    add js code for page

--- landing-page/README.md
+++ landing-page/README.md
@@ -1,13 +1,17 @@
-# Landing Page Project
+/**
+navigateTo(element)
+* @description scrolls an element into viewport
+* @param {html element} element
+*/
 
-## Table of Contents
+/**
+makeActive()
+* @description Checks if an element is in the viewport and adds the active class to it else removes the class
+*               also highlights the nav link to the section if in viewport.
+*                This is a callback function passed to an event listener for the document so that it is called whenever is scrolling.
+*/
 
-* [Instructions](#instructions)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+/**
+setLinkEvents()
+* @description Sets events for nav links to scroll the elements coresponding to each link into view
+*/
--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -17,41 +17,81 @@
  * Define Global Variables
  * 
 */
-
-
+const navList = document.getElementById('navbar__list');
+const sectionsList = document.querySelectorAll('section');
+const footer = document.querySelector('.page__footer');
 /**
  * End Global Variables
- * Start Helper Functions
  * 
+ * Start Helper Functions
+ *
 */
 
+
+const navigateTo = (element) => {
+    element.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
 
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
+//create li elements for every section and add a elements inside them with href set to coresponding section
+for (let i = 0; i < sectionsList.length; i++) {
+    navList.style = "display: none;";
+    const navItem = document.createElement('li');
+    const anchorItem = `<a id=${sectionsList[i].getAttribute('id').substring(7)} href="#${sectionsList[i].getAttribute('id')}">${sectionsList[i].getAttribute('data-nav')}</a>`;
+    navItem.innerHTML = anchorItem;
+    navItem.classList.add('menu__link');
+    navList.appendChild(navItem);
+    navList.style = "display: inherit;";
+}
 
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
+// Add class 'your-class-active' to section when near top of viewport
+const makeActive = () => {
+    for (const section of sectionsList) {
+        const box = section.getBoundingClientRect();
+        const linkActive = document.getElementById(section.getAttribute('id').substring(7)).parentElement;
+        if (box.top <= 200 && box.bottom >= 600) {
+            section.classList.add('your-active-class');
+            linkActive.classList.add('active__link');
+        } else {
+            section.classList.remove('your-active-class');
+            linkActive.classList.remove('active__link');
+        }
+    }
+}
+// Scroll to anchor ID using scrollIntoView
+function setLinkEvents() {
+    const links = document.querySelectorAll('a');
+    let targetIds = [];
+    for (let i = 0; i < links.length; i++) {
+        index = i + 1;
+        targetIds.push(`section${index}`);
+        links[i].addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetElement = document.getElementById(targetIds[i]);
+            navigateTo(targetElement);
+        });
+    }
+}
 
 
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
+document.addEventListener('scroll', makeActive);
+setLinkEvents();
 
-// Build menu 
 
-// Scroll to section on link click
 
-// Set sections as active
 
 
